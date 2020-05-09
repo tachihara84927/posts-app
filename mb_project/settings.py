7d8257c1b390815585719e58df341462f3b5198b
nekoGorilla <for.kaihatsu.168@gmail.com>
2020-05-10 01:47:51 +0900

    New Updates for Heroku

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7u9vum!)7r6$ne72xjoxa40g&&vjl^cpoxf0r!g)u5a_yg#-5a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
